1bd267244cf2dcb53ce271ffbac8d9499a55a69b
Joana Pereira <130710863+jcspereira76@users.noreply.github.com>
2023-05-11 00:00:43 +0100

    Add files via upload

--- src/writenoncanonical.c
+++ src/writenoncanonical.c
@@ -7,6 +7,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 
 #define BAUDRATE B38400
 #define MODEMDEVICE "/dev/ttyS1"
@@ -16,15 +17,124 @@
 
 volatile int STOP=FALSE;
 
+#define F 0x5C
+#define A 0x01
+#define C 0x07
+#define BCC A^C
+
+typedef enum {
+    START,
+    FLAG_RCV,
+    A_RCV,
+    C_RCV,
+    BCC_RCV,
+    END
+} UA_State;
+
+int UA_read(int fd)
+{
+    uint8_t temp;
+    int res;
+
+    UA_State currentState = START;
+
+    printf("Writing back UA\n");
+    while(currentState != END){
+
+         res = read(fd, &temp, 1);
+
+        if (res < 0) 
+            return NULL;  // erro
+ 
+        if (res == 0) {
+            if (currentState != END) 
+                return NULL;  // erro
+            else 
+                break;  // sucesso
+        }
+
+        switch(currentState){
+            case START:
+
+                if(temp == F){
+                    printf("\tReceived: %x\n", temp);
+                    currentState = FLAG_RCV;
+                }
+            break;
+            
+            case FLAG_RCV:
+
+                if(temp == F){
+                    printf("\tReceived: %x\n", temp);
+                    currentState = FLAG_RCV;
+                }
+                else if(temp == A){
+                    printf("\tReceived: 0%x\n", temp);
+                    currentState = A_RCV;
+                }
+                else
+                    currentState = START;
+                
+            break; 
+
+            case A_RCV:
+                
+                if(temp == F){
+                    printf("\tReceived: %x\n", temp);
+                    currentState = FLAG_RCV;
+                }
+                else if(temp == C){                  
+                    printf("\tReceived: 0%x\n", temp);
+                    currentState = C_RCV;
+                }
+                else 
+                    currentState = START;
+                
+            break;
+
+            case C_RCV:
+                
+                if(temp == F){
+                    printf("\tReceived: %x\n", temp);
+                    currentState = FLAG_RCV;
+                }
+                else if(temp == BCC){
+                    printf("\tReceived: 0%x\n", temp);
+                    currentState = BCC_RCV;
+                }
+                else 
+                    currentState = START;
+
+            break;
+
+            case BCC_RCV:
+
+                if(temp == F)
+                    currentState = END;
+                
+                else
+                    currentState = START;
+
+            break;
+
+            case END:
+            break; 
+        }
+    }
+
+    return 0;
+}
+
+
 int main(int argc, char** argv)
 {
-    int fd,c, res;
+    int fd,c, res, res_read;
     struct termios oldtio,newtio;
-    char buf[255];
+    char buf[255] = {0};
     int i, sum = 0, speed = 0;
 
     if ( (argc < 2) ||
-         ((strcmp("/dev/ttyS0", argv[1])!=0) &&
+         ((strcmp("/dev/ttyS10", argv[1])!=0) &&
           (strcmp("/dev/ttyS1", argv[1])!=0) )) {
         printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
         exit(1);
@@ -73,13 +183,37 @@ int main(int argc, char** argv)
 
     printf("New termios structure set\n");
 
+    //send SET
+    buf[0]=0x5C;
+    buf[1]=0x01;
+    buf[2]=0x03;
+    buf[3]=0x01^0x03;
+    buf[4]=0x5c;
+
+    res = write(fd,buf,strlen(buf));
+    printf("%d bytes written SET\n", res);
+    
+
+    res_read = UA_read(fd);
+
+    if(res_read == 0)
+        printf("Read UA correctly\n");
+    else 
+        printf("Error read UA\n");
+
+
+
+#if 1
     char ch;
 
+    printf("Write a message: ");
     for (i =0; (i<255) && ((ch = getchar()) != EOF) && (ch != '\n'); i++) 
         buf[i] = ch;
     buf[i] = '\0';
 
     printf("Writing \"%s\"\n", buf);
+
+
 
     // for (i = 0; i < 255; i++) {
     //     buf[i] = 'a';
@@ -91,15 +225,18 @@ int main(int argc, char** argv)
     res = write(fd,buf,strlen(buf) + 1);
     printf("%d bytes written\n", res);
 
+
     res = read(fd,buf,255);
 
     printf("Read %d bytes of a string of %d: %s\n", res, strlen(buf) + 1, buf);
-   
+
     /*
     O ciclo FOR e as instruções seguintes devem ser alterados de modo a respeitar
     o indicado no guião
     */
+#endif 
 
+    sleep(1);
 
     if ( tcsetattr(fd,TCSANOW,&oldtio) == -1) {
         perror("tcsetattr");
