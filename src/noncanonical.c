eb9164e538b0fdc97eab4c01c56eebbcbc7735d7
Joana Pereira <130710863+jcspereira76@users.noreply.github.com>
2023-05-13 23:59:39 +0100

    Add files via upload

--- src/noncanonical.c
+++ src/noncanonical.c
@@ -14,7 +14,7 @@
 #define FALSE 0
 #define TRUE 1
 
-volatile int STOP=FALSE;
+int STOP=FALSE;
 
 #define F 0x5C
 #define A 0x01
@@ -31,14 +31,50 @@ typedef enum {
     END
 } SET_State;
 
+int received_message(int fd)
+{
+    int i=0, res;
+    char msg[255], buf[255];
+
+    while (STOP==FALSE) {       
+        res = read(fd,buf,1);   
+                     
+        if (buf[0]=='\0') STOP=TRUE;
+        msg[i] = buf[0];
+        i++;
+    }
+
+    printf("Writing back\n"); 
+    res = write(fd, msg, i);
+    printf("\tMessage: %s :%d\n", msg, res);
+    
+    return 0;
+}
+
+int send_UA(int fd)
+{
+    int res;
+    char buf[255]={0};
+
+    buf[0]=0x5C;
+    buf[1]=0x01;
+    buf[2]=0x07;
+    buf[3]=0x01^0x07;
+    buf[4]=0x5c;
+
+    res = write(fd,buf,strlen(buf));
+    printf("%d bytes written UA\n", res);
+
+    return 0;
+}
+
 int SET_read(int fd)
 {
     uint8_t temp;
     int res;
 
     SET_State currentState = START;
-
-    printf("Writing back SET\n");
+   
     while(currentState != END){
 
         res = read(fd, &temp, 1);
@@ -57,7 +93,7 @@ int SET_read(int fd)
             case START:
 
                 if(temp == F){
-                    printf("\tReceived: %x\n", temp);
+                    printf("\tReceived: 0x%x\n", temp);
                     currentState = FLAG_RCV;
                 }
             break;
@@ -65,11 +101,11 @@ int SET_read(int fd)
             case FLAG_RCV:
 
                 if(temp == F){
-                     printf("\tReceived: %x\n", temp);
+                     printf("\tReceived: 0x%x\n", temp);
                     currentState = FLAG_RCV;
                 }
                 else if(temp == A){
-                     printf("\tReceived: 0%x\n", temp);
+                     printf("\tReceived: 0x0%x\n", temp);
                     currentState = A_RCV;
                 }
                 
@@ -81,11 +117,11 @@ int SET_read(int fd)
             case A_RCV:
                 
                 if(temp == F){
-                     printf("\tReceived: %x\n", temp);
+                     printf("\tReceived: 0x%x\n", temp);
                     currentState = FLAG_RCV;
                 }
                 else if(temp == C){
-                    printf("\tReceived: 0%x\n", temp);
+                    printf("\tReceived: 0x0%x\n", temp);
                     currentState = C_RCV;
                 }
                 else 
@@ -96,11 +132,11 @@ int SET_read(int fd)
             case C_RCV:
                 
                 if(temp == F){
-                     printf("\tReceived: %x\n", temp);
+                     printf("\tReceived: 0x%x\n", temp);
                     currentState = FLAG_RCV;
                 }
                 else if(temp == BCC){
-                    printf("\tReceived: 0%x\n", temp);
+                    printf("\tReceived: 0x0%x\n", temp);
                     currentState = BCC_RCV;
                 }
                 else 
@@ -127,7 +163,7 @@ int SET_read(int fd)
 
 int main(int argc, char** argv)
 {
-    int fd, c, res, res_read;
+    int fd, c, res, res_read, res_write;
     struct termios oldtio,newtio;
     char buf[255];
 
@@ -179,7 +215,7 @@ int main(int argc, char** argv)
 
     printf("New termios structure set\n");
 
-    
+    printf("Writing back SET\n");
     res_read = SET_read(fd);
 
     if(res_read == 0)
@@ -187,36 +223,14 @@ int main(int argc, char** argv)
     else 
         printf("Error read SET\n");
 
-    //Send UA
-    buf[0]=0x5C;
-    buf[1]=0x01;
-    buf[2]=0x07;
-    buf[3]=0x01^0x07;
-    buf[4]=0x5c;
+    res_write = send_UA(fd);
+    if (res_write != 0)
+        printf("Error sending SET\n");
 
-    res = write(fd,buf,strlen(buf));
-    printf("%d bytes written UA\n", res);
 
- #if 1  
-    int i=0;
-    char msg[255];
-
-    while (STOP==FALSE) {       /* loop for input */
-        res = read(fd,buf,1);   /* returns after 5 chars have been input */
-        //buf[res]='\0';               /* so we can printf... */
-        if (buf[0]=='\0') STOP=TRUE;
-        msg[i] = buf[0];
-        i++;
-    }
-
-        printf("Writing back\n"); 
-        res = write(fd, msg, i);
-        printf("\tMessage: %s :%d\n", msg, res);
-
-    /*
-    O ciclo WHILE deve ser alterado de modo a respeitar o indicado no guião
-    */
-#endif 
+    res = received_message(fd);
+    if(res != 0)
+        printf("Error received message\n");
 
     sleep(1);
     tcsetattr(fd,TCSANOW,&oldtio);
--- src/writenoncanonical.c
+++ src/writenoncanonical.c
@@ -8,6 +8,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
+#include <signal.h>
 
 #define BAUDRATE B38400
 #define MODEMDEVICE "/dev/ttyS1"
@@ -15,7 +16,8 @@
 #define FALSE 0
 #define TRUE 1
 
-volatile int STOP=FALSE;
+int STOP=FALSE;
+int fd;
 
 #define F 0x5C
 #define A 0x01
@@ -31,17 +33,58 @@ typedef enum {
     END
 } UA_State;
 
+
+int send_message(int fd)
+{
+    int res, i;
+    char ch, buf[255];
+
+    printf("Write a message: ");
+    for (i =0; (i<255) && ((ch = getchar()) != EOF) && (ch != '\n'); i++) 
+        buf[i] = ch;
+    buf[i] = '\0';
+
+    printf("Writing \"%s\"\n", buf);
+ 
+    res = write(fd,buf,strlen(buf) + 1);
+    printf("%d bytes written\n", res);
+
+
+    res = read(fd,buf,255);
+
+    printf("Read %d bytes of a string of %d: %s\n", res, strlen(buf) + 1, buf);
+
+    return 0;
+}
+
+int send_SET(int fd)
+{
+    int res;
+    char buf[255]={0};
+
+    buf[0]=0x5C;
+    buf[1]=0x01;
+    buf[2]=0x03;
+    buf[3]=0x01^0x03;
+    buf[4]=0x5c;
+
+    res = write(fd,buf,strlen(buf));
+    printf("%d bytes written SET\n", res);
+
+    return 0;   
+}
+
 int UA_read(int fd)
 {
-    uint8_t temp;
+    uint8_t temp = 0;
     int res;
 
     UA_State currentState = START;
 
-    printf("Writing back UA\n");
+    //printf("Writing back UA\n");
     while(currentState != END){
 
-         res = read(fd, &temp, 1);
+        res = read(fd, &temp, 1);
 
         if (res < 0) 
             return NULL;  // erro
@@ -56,23 +99,24 @@ int UA_read(int fd)
         switch(currentState){
             case START:
 
-                if(temp == F){
-                    printf("\tReceived: %x\n", temp);
+                if(temp == F){                    
+                    printf("\tReceived: 0x%x\n", temp);
                     currentState = FLAG_RCV;
                 }
+
             break;
             
             case FLAG_RCV:
 
                 if(temp == F){
-                    printf("\tReceived: %x\n", temp);
+                    printf("\tReceived: 0x%x\n", temp);
                     currentState = FLAG_RCV;
                 }
                 else if(temp == A){
-                    printf("\tReceived: 0%x\n", temp);
+                    printf("\tReceived: 0x0%x\n", temp);
                     currentState = A_RCV;
                 }
-                else
+                else                    
                     currentState = START;
                 
             break; 
@@ -80,11 +124,11 @@ int UA_read(int fd)
             case A_RCV:
                 
                 if(temp == F){
-                    printf("\tReceived: %x\n", temp);
+                    printf("\tReceived: 0x%x\n", temp);
                     currentState = FLAG_RCV;
                 }
                 else if(temp == C){                  
-                    printf("\tReceived: 0%x\n", temp);
+                    printf("\tReceived: 0x0%x\n", temp);
                     currentState = C_RCV;
                 }
                 else 
@@ -95,11 +139,11 @@ int UA_read(int fd)
             case C_RCV:
                 
                 if(temp == F){
-                    printf("\tReceived: %x\n", temp);
+                    printf("\tReceived: 0x%x\n", temp);
                     currentState = FLAG_RCV;
                 }
                 else if(temp == BCC){
-                    printf("\tReceived: 0%x\n", temp);
+                    printf("\tReceived: 0x0%x\n", temp);
                     currentState = BCC_RCV;
                 }
                 else 
@@ -125,13 +169,31 @@ int UA_read(int fd)
     return 0;
 }
 
+void alarme()
+{
+    int res;
+    char buf[255]={0};
+    //send SET
+    buf[0]=0x5C;
+    buf[1]=0x01;
+    buf[2]=0x03;
+    buf[3]=0x01^0x03;
+    buf[4]=0x5c;
+
+    printf("Don't recieve UA\n");
+    res = write(fd,buf,strlen(buf));
+    printf("%d bytes written SET\n", res);
+
+    alarm(3);
+}
+
 
 int main(int argc, char** argv)
 {
-    int fd,c, res, res_read;
+    int c, res, res_read, res_write;
     struct termios oldtio,newtio;
     char buf[255] = {0};
-    int i, sum = 0, speed = 0;
+    int sum = 0, speed = 0;
 
     if ( (argc < 2) ||
          ((strcmp("/dev/ttyS10", argv[1])!=0) &&
@@ -183,18 +245,17 @@ int main(int argc, char** argv)
 
     printf("New termios structure set\n");
 
-    //send SET
-    buf[0]=0x5C;
-    buf[1]=0x01;
-    buf[2]=0x03;
-    buf[3]=0x01^0x03;
-    buf[4]=0x5c;
+    (void) signal(SIGALRM, alarme);
 
-    res = write(fd,buf,strlen(buf));
-    printf("%d bytes written SET\n", res);
-    
+    res_write = send_SET(fd);
+    if (res_write != 0)
+        printf("Error sending SET\n");
+
+    alarm(3);
+   
 
     res_read = UA_read(fd);
+    alarm(0);
 
     if(res_read == 0)
         printf("Read UA correctly\n");
@@ -202,39 +263,9 @@ int main(int argc, char** argv)
         printf("Error read UA\n");
 
 
-
-#if 1
-    char ch;
-
-    printf("Write a message: ");
-    for (i =0; (i<255) && ((ch = getchar()) != EOF) && (ch != '\n'); i++) 
-        buf[i] = ch;
-    buf[i] = '\0';
-
-    printf("Writing \"%s\"\n", buf);
-
-
-
-    // for (i = 0; i < 255; i++) {
-    //     buf[i] = 'a';
-    // }
-
-    /*testing*/
-    // buf[25] = '\n';
-
-    res = write(fd,buf,strlen(buf) + 1);
-    printf("%d bytes written\n", res);
-
-
-    res = read(fd,buf,255);
-
-    printf("Read %d bytes of a string of %d: %s\n", res, strlen(buf) + 1, buf);
-
-    /*
-    O ciclo FOR e as instruções seguintes devem ser alterados de modo a respeitar
-    o indicado no guião
-    */
-#endif 
+    res = send_message(fd);
+    if(res != 0)
+        printf("Error sending message\n");
 
     sleep(1);
 
